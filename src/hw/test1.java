09b8c92cc7f1f0defa4558b8fe8a71dc72f56ccf
hw666 <935610879@qq.com>
2018-03-12 21:55:49 +0800

    This is a test file
    nihao,zt

---
+++ src/hw/test1.java
@@ -0,0 +1,69 @@
+package hw;
+
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class test1 {
+	public static void main(String[] args) {
+		final double PI = 3.14159;
+		Scanner input = new Scanner(System.in);
+
+		/*
+		 * System.out.print("shruyigedoubleleixing"); double r= input.nextDouble();
+		 * 
+		 * double area=r*r*PI;
+		 * 
+		 * System.out.print("shuchuminajishi:"+ "area is "+area);
+		 */
+		/*
+		 * System.out.print("Input a integer for seconds:"); int sec= input.nextInt();
+		 * 
+		 * int min = sec/60; int sec2 = sec%60;
+		 * 
+		 * System.out.println("The time is "+ min+"minute"+sec2+"seconds");
+		 */
+
+		/*
+		 * long totalmillseconds = System.currentTimeMillis();
+		 * 
+		 * long totalsec = totalmillseconds / 1000;
+		 * 
+		 * long currentsec = totalsec % 60;
+		 * 
+		 * long totalmin = totalsec / 60;
+		 * 
+		 * long currentmin = totalmin % 60;
+		 * 
+		 * long totalhour = totalmin / 60;
+		 * 
+		 * long currenthour = totalhour % 24;
+		 * 
+		 * System.out.println("Current time is " + currenthour + ":" + currentmin + ":"
+		 * + currentsec);
+		 */
+
+		/*
+		 * String str1 = input.next(); String str2 = input.next(); String str3 =
+		 * input.next();
+		 * 
+		 * System.out.println(str1); System.out.println(str2); System.out.println(str3);
+		 */
+
+		/*
+		 * int year = input.nextInt(); boolean isleapyear = (year % 4 == 0 && year % 100
+		 * != 0) || (year % 400 == 0);
+		 * 
+		 * System.out.println(year + "is a leapyear?" + isleapyear);
+		 * 
+		 * 
+		 * int r = (int) (Math.random() * 100);
+		 * 
+		 * System.out.println(r);
+		 */
+		
+		
+		
+		
+
+	}
+}
